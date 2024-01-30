59f4761f943abf599d317c5e2ba68ffd65dcfd31
Jonathan Augustin <tun45218@temple.edu>
2024-01-30 12:36:17 -0500

    Elements constrained; Name is dependent

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.helloworld
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,7 +10,9 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
+    lateinit var clickButton: Button
     lateinit var displayTextView: TextView
+    lateinit var nameEdit: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,10 +20,12 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        clickButton = findViewById(R.id.clickMeButton)
+        nameEdit = findViewById(R.id.nameEditText)
 
-        
         // Respond to button click event per specifications
-
-
+        clickButton.setOnClickListener {
+            displayTextView.setText ("Hello, ${nameEdit.text}!")
+        }
     }
 }
